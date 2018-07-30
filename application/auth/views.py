7114c4f005ff1f910445da4e05c5213c66629f18
Lilja Metsälampi <lilja.metsalampi@helsinki.fi>
2018-07-30 12:03:50 +0300

    poistettiin kommentoitua koodia

--- application/auth/views.py
+++ application/auth/views.py
@@ -2,17 +2,16 @@ from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
 from application import app
-from application.auth.models import User
 from application.auth.forms import LoginForm
+from application.auth.models import User
 
-@app.route("/auth/login", methods = ["GET", "POST"])
+
+@app.route("/auth/login", methods=["GET", "POST"])
 def auth_login():
     if request.method == "GET":
-        return render_template("auth/loginform.html", form = LoginForm())
+        return render_template("auth/loginform.html", form=LoginForm())
 
     form = LoginForm(request.form)
-    # if form.username.data == '':
-    #    return render_template("auth/loginform.html", form = form)
 
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
     if not user:
@@ -21,6 +20,7 @@ def auth_login():
 
     login_user(user)
     return redirect(url_for("index"))
+
 
 @app.route("/auth/logout")
 def auth_logout():
