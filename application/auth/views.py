ce7ee736d3717fe873239e1db506cd6ecc845e67
Lilja Metsälampi <lilja.metsalampi@helsinki.fi>
2018-07-30 13:08:51 +0300

    rekisteröityminen toimii

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.forms import LoginForm
+from application.auth.forms import RegistrationForm
 from application.auth.models import User
 
 
@@ -25,4 +26,33 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/auth/register", methods=["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("auth/registrationform.html", form=RegistrationForm())
+
+    form = RegistrationForm(request.form)
+
+    user = User.query.filter_by(username=form.username.data).first()
+    if user:
+        return render_template("auth/registrationform.html", form=form,
+                               error="User already exists")
+
+    if form.username.data == '':
+        return render_template("auth/registrationform.html", form=form,
+                               error="Username empty")
+
+    if form.password.data == '':
+        return render_template("auth/registrationform.html", form=form,
+                               error="Password empty")
+
+    user = User(form.name.data, form.username.data, form.password.data)
+
+    db.session().add(user)
+    db.session().commit()
+
+    login_user(user)
     return redirect(url_for("index"))
