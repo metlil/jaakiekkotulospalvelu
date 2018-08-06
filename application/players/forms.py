74d8917575c003b1ba8e9e3ff9c0dcb95e4d7a35
Lilja Metsälampi <lilja.metsalampi@helsinki.fi>
2018-08-06 17:59:52 +0300

    pelaajat CRUD-toiminnallisuus toimii

--- application/players/forms.py
+++ application/players/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, SubmitField
 
 class PlayerForm(FlaskForm):
     firstname = StringField("Firstname")
     lastname = StringField("Lastname")
     number = IntegerField("Number")
+
+    delete = SubmitField("Delete")
 
     class Meta:
         csrf = False
--- application/players/views.py
+++ application/players/views.py
@@ -1,12 +1,13 @@
 from flask import render_template, request, redirect, url_for
 
 from application import app, db
-from application.players.models import Player
 from application.players.forms import PlayerForm
+from application.players.models import Player
+
 
 @app.route("/players/new/")
 def players_form():
-    return render_template("players/new.html", form = PlayerForm())
+    return render_template("players/new.html", form=PlayerForm())
 
 
 @app.route("/players/", methods=["POST"])
@@ -15,12 +16,50 @@ def players_create():
 
     p = Player(form.firstname.data, form.lastname.data, form.number.data)
 
-
     db.session().add(p)
     db.session().commit()
 
     return redirect(url_for("players_index"))
 
+
 @app.route("/players/", methods=["GET"])
 def players_index():
-    return render_template("players/list.html", players = Player.query.all())
+    return render_template("players/list.html", players=Player.query.all())
+
+@app.route("/players/<player_id>/", methods=["GET", "POST", "DELETE"])
+def player_page(player_id):
+    if request.method == 'POST':
+        return players_modify(player_id)
+    else:
+        return players_update_form(player_id)
+
+
+@app.route("/players/<player_id>/delete", methods=["POST"])
+def player_delete(player_id):
+    player = Player.query.get(player_id)
+    db.session().delete(player)
+    db.session().commit()
+    return redirect(url_for("players_index"))
+
+
+def players_update_form(player_id):
+    player = Player.query.get(player_id)
+    form = PlayerForm()
+    form.firstname.data = player.firstname
+    form.lastname.data = player.lastname
+    form.number.data = player.number
+    return render_template("players/update.html", form=form, player_id=player_id)
+
+
+def players_modify(player_id):
+    form = PlayerForm(request.form)
+
+    p = Player(form.firstname.data, form.lastname.data, form.number.data)
+    p2 = Player.query.get(player_id)
+    p2.firstname = p.firstname
+    p2.lastname = p.lastname
+    p2.number = p.number
+
+    db.session().commit()
+
+    return redirect(url_for("players_index"))
