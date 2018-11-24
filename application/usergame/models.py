d8f95ee9fc8ddf7f75729de73711ac4a284f6564
Lilja Metsälampi <lilja.metsalampi@helsinki.fi>
2018-11-24 16:53:49 +0200

    etusivulla näkyy lista käyttäjän peleistä, joista linkit pelisivuille

--- application/usergame/models.py
+++ application/usergame/models.py
@@ -1,4 +1,8 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy import text
+
 from application import db
+
 
 class UserGame(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,3 +15,49 @@ class UserGame(db.Model):
     def __init__(self, user_id, game_id):
         self.user_id = user_id
         self.game_id = game_id
+
+    @staticmethod
+    def get_games_for_user(user_id):
+        stmt = text(
+            "SELECT"
+            "  HomeTeam.name home_name,"
+            "  GuestTeam.name guest_name,"
+            "  Game.time,"
+            "  Game.place,"
+            "  Game.status,"
+            "  Game.id "
+            "FROM"
+            "  ("
+            "    SELECT"
+            "      Game.home_id,"
+            "      Game.guest_id,"
+            "      Game.time,"
+            "      Game.place,"
+            "      Game.status,"
+            "      Game.id "
+            "    FROM"
+            "      User_Game"
+            "      INNER JOIN Game"
+            "        ON User_Game.game_id = Game.id "
+            "    WHERE"
+            "      User_Game.user_id = :user_id"
+            "  ) Game,"
+            "  Team HomeTeam,"
+            "  Team GuestTeam "
+            "WHERE"
+            "  Game.home_id = HomeTeam.id"
+            "  AND Game.guest_id = GuestTeam.id "
+            "ORDER BY Game.time"
+        ).params(
+            user_id=user_id
+        ).columns(
+            home_name=String, guest_name=String,
+            place=String, status=String, time=DateTime)
+
+        res = db.engine.execute(stmt)
+        response = []
+        for row in res:
+            response.append({"home_name": row[0], "guest_name": row[1], "time": row[2],
+                             "place": row[3], "status": row[4], "game_id":row[5]})
+
+        return response
--- application/views.py
+++ application/views.py
@@ -1,9 +1,15 @@
+from flask_login import current_user
+
 from application import app, get_render_page_function
 from application.stats.models import standings
+from application.usergame.models import UserGame
 
 render_page = get_render_page_function('index')
 
 
 @app.route("/")
 def index():
-    return render_page("index.html", standings=standings())
+    usergames = []
+    if current_user.is_authenticated:
+        usergames = UserGame.get_games_for_user(current_user.id)
+    return render_page("index.html", standings=standings(), usergames=usergames)
